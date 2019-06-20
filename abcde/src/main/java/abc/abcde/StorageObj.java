5da823bbf45e08eb4577e17aaeb27f72031748d9
JOAO OCTAVIO FREITAS BITELO <16111210@pucrs.br>
2019-06-19 22:28:09 -0300

    começo da normalização

---
+++ abcde/src/main/java/abc/abcde/StorageObj.java
@@ -0,0 +1,33 @@
+package abc.abcde;
+
+public class StorageObj {
+	private String quesion;
+	private String answer;
+	private String clas;
+	private String questionNormalized;
+
+	public String getQuesion() {
+		return quesion;
+	}
+	public void setQuesion(String quesion) {
+		this.quesion = quesion;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	public String getClas() {
+		return clas;
+	}
+	public void setClas(String clas) {
+		this.clas = clas;
+	}
+	public String getQuestionNormalized() {
+		return questionNormalized;
+	}
+	public void setQuestionNormalized(String questionNormalized) {
+		this.questionNormalized = questionNormalized;
+	}
+}
